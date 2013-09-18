995dcd0d98810b4670862bf2d97523b511487dbc
wanderercat <1one1un2two2deux3three3trois@gmail.com>
2013-09-18 12:58:53 +0900

    バグ修正
    タッチイベントを検出できていないバグを修正
    このアプリケーションのManifest.xmlではminSDKversionが8に設定されていますが、API level 5以降ではgetAction()の戻り値をそのまま使用してもイベントコードが取得出来ないので、getAction()の戻り値をMotionEvent.ACTION_MASKとの論理積でマスクさせてイベントコードを取得できるように修正致しました。

--- src/com/example/soundsample001/SoundSample001.java
+++ src/com/example/soundsample001/SoundSample001.java
@@ -18,32 +18,60 @@ public class SoundSample001 extends Activity {
 	@Override
 	public void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
+		//描画クラスのインスタンスを生成
 		MyView mView = new MyView( this );
+		//現在のViewに設定
 		setContentView( mView );
 	}
 }
 
+/**
+* 描画用のクラス
+*/
 class MyView extends View {
+    /** サウンド再生データを保持する 。*/
 	private MediaPlayer mp;
-
+    
+	/**
+	* コンストラクタ
+	* ＠param context コンテキスト
+	*/
 	public MyView( Context context ) {
 		super( context );
+		
+		// イベントが取得できるようにFocusを有効にする
 		setFocusable( true );
+		
+		// サウンドデータを読み込む (/res/raw/pon.mp3)
 		mp = MediaPlayer.create( context, R.raw.pon );
 	}
-
+    
+	/**
+	* 描画処理。
+	*/
 	@Override
 	protected void onDraw( Canvas canvas ) {
+	    // 背景色を設定する
 		canvas.drawColor( Color.BLUE );
 	}
-
+    /**
+	* タッチイベント。
+	*/
 	@Override
 	public boolean onTouchEvent( MotionEvent event ) {
-		if ( event.getAction() == MotionEvent.ACTION_DOWN ) {
-			mp.seekTo( 0 );
-			mp.start();
-		} else if ( event.getAction() == MotionEvent.ACTION_UP ) {
+	//        event.getActionMasked() //API8
+		if (( event.getAction() & MotionEvent.ACTION_MASK ) == MotionEvent.ACTION_DOWN ) {  //API8以前(API5~7)でもOK
+		           // ACTION_DOWNは指が触れた時
+				   // 音の再生開始位置を0ミリセカンドの位置に設定する
+			       mp.seekTo( 0 );
+			      // 音の再生を開始する
+		     	  mp.start();
+		} else if (( event.getAction() & MotionEvent.ACTION_MASK ) == MotionEvent.ACTION_UP ) { //API8以前(API5~7)でもOK
+		          // ACTION_UPは指が離れた時
+				  // 音を停止する
 			mp.stop();
+			
+			      //一度再生をstop()してから再び音を再生する場合は、prepare()を呼び出す必要がある
 			try {
 				mp.prepare();
 			} catch ( IllegalStateException e ) {
